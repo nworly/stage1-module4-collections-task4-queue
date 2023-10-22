bb9a15b865d8a9126d02a3bb6f721299ca8c43e0
Liza Etkind <lizaetkind@gmail.com>
2023-10-22 18:11:13 +0300

    Queue Interface tasks completed

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        int count = 1;
+        while (!queue.isEmpty()) {
+            if (count == everyDishNumberToEat) {
+                list.add(queue.poll());
+                count = 1;
+            } else {
+                queue.add(queue.poll());
+                count++;
+            }
+        }
+
+        return list;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,14 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        queue.addAll(firstList);
+        queue.addAll(secondList);
+
+        return queue;
     }
 }
